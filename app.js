6a5dcc0dda0c14281779c23356af05780701e92b
stevengalvis <steven.galvis@gmail.com>
2017-04-16 18:02:43 -0400

    wordcount function

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// your code here!
+function convertText(text){
+    var textArray = text.toString().toLowerCase();
+    return textArray.split(' ');
+}
+
+function totalWordCount(text) {
+    var wordCount = convertText(text);
+    console.log(wordCount);
+    return wordCount.length;
+}
+
+console.log(totalWordCount('this is just a test'));
+console.log(totalWordCount('lol'));
