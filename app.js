69c65fefa096f927bffb7abc1ac6badaa6450792
stevengalvis <steven.galvis@gmail.com>
2017-04-17 08:06:48 -0400

    added uniqueWordCount function

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function convertText(text){
-    var textArray = text.toString().toLowerCase();
+    var textArray = text.toString().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
     return textArray.split(' ');
 }
 
@@ -9,5 +9,23 @@ function totalWordCount(text) {
     return wordCount.length;
 }
 
-console.log(totalWordCount('this is just a test'));
+console.log(totalWordCount('this is just a test,(dkkd)###, i want 838484 to keep testing'));
 console.log(totalWordCount('lol'));
+
+function uniqueWordCount(text){
+    var checkCount = convertText(text);
+    var wordCount = {};
+    for(var i = 0;  i<checkCount.length; i++){
+        if (wordCount.hasOwnProperty([checkCount[i]])){
+            wordCount[checkCount[i]] += 1;
+
+        }
+        else {
+            wordCount[checkCount[i]] = 1;
+
+        }
+    }
+    return wordCount;
+
+}
+console.log(uniqueWordCount('this is another test this this, this'));
