83c8b230168beae13759df15bad580a998eb670d
stevengalvis <steven.galvis@gmail.com>
2017-04-19 06:17:54 -0400

    added jquery events

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-function convertText(text){
-    var textArray = text.toString().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+function convertText(text) {
+    var textArray = text.toString().toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
     return textArray.split(' ');
 }
 
@@ -8,38 +8,48 @@ function totalWordCount(text) {
     return wordCount.length;
 }
 
-
-function getUniqueWords(text){
+function getUniqueWords(text) {
     var checkCount = convertText(text);
     var uniqueWords = {};
-    for(var i = 0;  i<checkCount.length; i++){
-        if (uniqueWords.hasOwnProperty([checkCount[i]])){
+    for (var i = 0; i < checkCount.length; i++) {
+        if (uniqueWords.hasOwnProperty([checkCount[i]])) {
             uniqueWords[checkCount[i]] += 1;
-        }
-        else {
+        } else {
             uniqueWords[checkCount[i]] = 1;
         }
     }
     return uniqueWords;
-
-
 }
 
-function uniqueWordCount(text){
+function uniqueWordCount(text) {
     var listOfWords = getUniqueWords(text);
     return Object.keys(listOfWords).length;
 };
 
-function averageWordLength(text){
+function averageWordLength(text) {
     var uniqueWords = getUniqueWords(text)
     var uniqueWordsArray = Object.keys(uniqueWords);
     var totalLength = 0;
-    for(word in uniqueWords){
+    for (word in uniqueWords) {
         totalLength += word.length;
     }
     return totalLength / uniqueWordCount(text);
-
 }
 
+function analyzeText(){
+    $('button').on('click',function(e){
+        e.preventDefault();
+        //get words from text area
+        var $text = $('textarea').val();
+        $('dl').removeClass('hidden');
+        $('.js-word-count').after(totalWordCount($text));
+        $('.js-unique-word-count').after(uniqueWordCount($text));
+        $('.js-average-word').after(averageWordLength($text));
+        $text.val('');
+    });
+}
 
-console.log(averageWordLength('this is another bleh lol test this this this'));
+$(function() {
+    //event listener function
+    analyzeText();
+});
